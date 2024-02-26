711ead64b13c6d62e57242c6c47b93de1b16dae2
mandimko <160102089+mandimko@users.noreply.github.com>
2024-02-26 19:45:40 +0200

    new search engine

---
+++ index.js
@@ -0,0 +1,6 @@
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+}
