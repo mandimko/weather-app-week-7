73605851d2878341b43d615d90893ec1f4c786f3
mandimko <160102089+mandimko@users.noreply.github.com>
2024-03-05 13:30:27 +0200

    update search engine

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
+function updateWeatherInfo(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHtml = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "oc97bb81ta3dfb63a40dcf330560f151";
+  let apiUrl ='https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric';
+  axios.get(apiUrl).then(updateWeatherInfo);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
-let form = document.querySelector("form");
-form.addEventListener("submit", handleSearchSubmit);
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+searchCity("paris");
