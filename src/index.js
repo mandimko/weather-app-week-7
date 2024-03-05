3e71634ad8a971341cd8799b6b5829a5e085f65a
mandimko <160102089+mandimko@users.noreply.github.com>
2024-03-05 16:41:12 +0200

    fixed apiURL

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ function updateWeatherInfo(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
-  cityElement.innerHtml = response.data.city;
+  cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
 }
 
 function searchCity(city) {
   let apiKey = "oc97bb81ta3dfb63a40dcf330560f151";
-  let apiUrl ='https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric';
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`;
   axios.get(apiUrl).then(updateWeatherInfo);
 }
 
